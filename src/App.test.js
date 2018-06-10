a9e8b2c155e4dc0295cf1aa18678139d6f8bf46b
Wolfgang Becker <wolli.becker@gmail.com>
2018-06-10 23:44:04 +0200

    Wrap App tests in describe block

--- src/App.test.js
+++ src/App.test.js
@@ -9,17 +9,19 @@ import BookSearch from './BookSearch';
 
 jest.mock('./BooksAPI', () => 'BooksAPI')
 
-it('has 2 routes', () => {
-  const wrapper = shallow(<App />)
-  expect(wrapper.find(Route).length).toBe(2)
-});
+describe('App', function () {
+  it('has 2 routes', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find(Route).length).toBe(2)
+  });
 
-it('routes to BookShelfList', () => {
-  const wrapper = shallow(<App />)
-  expect(wrapper.find(Route).first().props().component).toEqual(BookShelfList)
-});
+  it('routes to BookShelfList', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find(Route).first().props().component).toEqual(BookShelfList)
+  });
 
-it('routes to BookSearch', () => {
-  const wrapper = shallow(<App />)
-  expect(wrapper.find(Route).last().props().component).toEqual(BookSearch)
+  it('routes to BookSearch', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find(Route).last().props().component).toEqual(BookSearch)
+  });
 });
