8a8568a5c2227ad5f6ce089575a2671236ca8880
Wolfgang Becker <wolli.becker@gmail.com>
2018-06-10 21:24:12 +0200

    Test App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Route } from 'react-router-dom';
 
-/** 
- This course is not designed to teach Test Driven Development. 
- Feel free to use this file to test your application, but it 
- is not required.
-**/
+import * as BooksAPI from './BooksAPI'
+import App from './App';
+import BookShelfList from './BookShelfList';
+import BookSearch from './BookSearch';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-})
+jest.mock('./BooksAPI', () => 'BooksAPI')
 
+it('has 2 routes', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.find(Route).length).toBe(2)
+});
 
+it('routes to BookShelfList', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.find(Route).first().props().component).toEqual(BookShelfList)
+});
+
+it('routes to BookSearch', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.find(Route).last().props().component).toEqual(BookSearch)
+});
