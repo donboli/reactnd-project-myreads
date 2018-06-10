875acd3c41313718c3a41ff3563edebc1a289db9
Wolfgang Becker <wolli.becker@gmail.com>
2018-06-10 12:33:23 +0200

    Enable routing through react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import BookSearch from './BookSearch'
 import BookShelfList from './BookShelfList'
+import { Route } from 'react-router-dom';
 import './App.css'
 
 const shelves = [
@@ -333,24 +334,13 @@ const books = [
 ]
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <BookSearch />
-        ) : (
+        <Route exact path='/' render={() => (
           <BookShelfList shelves={shelves} books={books}/>
-        )}
+        )} />
+        <Route path='/search' component={BookSearch} />
       </div>
     )
   }
--- src/BookShelfList.js
+++ src/BookShelfList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import BookShelf from './BookShelf'
+import { Link } from 'react-router-dom';
 
 class BookShelfList extends Component {
   render() {
@@ -21,7 +22,7 @@ class BookShelfList extends Component {
           </div>
         </div>
         <div className="open-search">
-          <a>Add a book</a>
+          <Link to='search'>Add a book</Link>
         </div>
       </div>
     )
